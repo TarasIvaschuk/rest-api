c46ed9d08f74fa28a2d8f2cb7d34908cc23fef9a
Taras Ivashchuk <tarasivashchuk@gmail.com>
2022-05-24 19:41:35 +0300

    added CORS

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+  next();
+});
+
 const feedRoutes = require("./routes/feed");
 
 app.use("/feed", feedRoutes);
