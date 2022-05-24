115e8a335d2d7a030638601e0894586acbf7646c
taras <tarasivashchuk@gmail.com>
2022-05-24 13:58:24 +0300

    added createPost API

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,17 @@
 exports.getPosts = (req, res, next) => {
-  console.log("getPosts");
+  return res.status(200).json({post: [{title: "My first post"}, {content: "Hello World"}]});
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.title;
+
+  return res.status(201).json({
+    message: "Post created successfully",
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content
+    }
+  });
 };
