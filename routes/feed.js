a2913671ba87d5f27f7fdf030715b0e6e236ab22
taras <tarasivashchuk@gmail.com>
2022-05-28 01:13:23 +0300

    reformatted

--- routes/feed.js
+++ routes/feed.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const feedController = require("../controllers/feed");
-const {body} = require("express-validator");
-
+const { body } = require("express-validator");
 
 router.get("/posts", feedController.getPosts);
 router.post("/post", [
-  body('title')
+  body("title")
     .trim()
-    .isLength({min:7}),
-  body('content')
+    .isLength({ min: 7 }),
+  body("content")
     .trim()
-    .isLength({min:7})
-],feedController.createPost);
+    .isLength({ min: 7 })
+], feedController.createPost);
 
 module.exports = router;
