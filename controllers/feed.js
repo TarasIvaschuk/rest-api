0c6f59240b8ff7b64832459b112161f7d895d349
taras <tarasivashchuk@gmail.com>
2022-05-28 01:18:00 +0300

    added possibility to save post to MongoDB

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   return res.status(200).json({
@@ -14,32 +15,38 @@ exports.getPosts = (req, res, next) => {
         createdAt: new Date()
       }
     ]
-  })
-    ;
+  });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
-  const title = req.body.title;
-  const content = req.body.content;
+  const { title, content } = req.body;
 
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     return res.status(422).json({
       message: "Validation failed, entered data is incorrect",
       errors: errors.array()
-    })
+    });
   }
 
-  return res.status(201).json({
-    message: "Post created successfully",
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: {
-        name: "Taras"
-      },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title,
+    content,
+    imageUrl: "images/duck.jpg",
+    creator: {
+      name: "Taras"
+    },
   });
+
+  post.save()
+    .then((result) => {
+      console.log(result);
+      return res.status(201).json({
+        message: "Post created successfully",
+        post: result
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
