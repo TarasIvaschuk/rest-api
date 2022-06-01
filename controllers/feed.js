5e17df315594dec27606156305bbd04d7c95c86e
taras <tarasivashchuk@gmail.com>
2022-06-01 21:00:10 +0300

    added 'update post' support

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
+const fs = require("fs");
+const path = require("path");
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -16,10 +18,9 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  const errors = validationResult(req);
   const { title, content } = req.body;
-  console.log(req);
 
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const err = new Error("Validation failed, entered data is incorrect");
     err.statusCode = 422;
@@ -42,7 +43,6 @@ exports.createPost = (req, res, next) => {
     },
   });
 
-  // noinspection JSUnresolvedFunction
   post.save()
     .then((result) => {
       console.log(result);
@@ -76,4 +76,59 @@ exports.getPost = (req, res, next) => {
         return next(err);
       }
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Validation failed, entered data is incorrect");
+    err.statusCode = 422;
+    return next(err);
+    // throw error;
+  }
+  const postId = req.params.postId;
+  const content = req.body.content;
+  const title = req.body.title;
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const err = new Error("No file picked!");
+    err.statusCode = 422;
+    throw err;
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const err = new Error("Could not find the post");
+        err.statusCode = 404;
+        throw err;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then((result) =>
+      res.status(200).json({
+        message: "Post updated!",
+        post: result
+      }))
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        return next(err);
+      }
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
