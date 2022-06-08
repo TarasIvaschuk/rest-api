f91f771d5781ee52459a4b4eed8840d0b010bf4b
taras <tarasivashchuk@gmail.com>
2022-06-08 18:39:11 +0300

    added encryption added signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -12,4 +13,25 @@ exports.signup = (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
+  bcrypt.hash(password, 12)
+    .then(hashedPsw => {
+      const user = new User({
+        email,
+        password: hashedPsw,
+        name,
+      });
+      return user.save();
+    })
+    .then(result => {
+      return res.status(201).json({
+        message: "User is created",
+        userId: result._id
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        return next(err);
+      }
+    });
 };
