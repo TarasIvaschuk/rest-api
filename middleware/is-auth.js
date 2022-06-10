52122e6750cc6ce9ab5efec42fc2fc7417c4a714
taras <tarasivashchuk@gmail.com>
2022-06-10 15:25:07 +0300

    added authorization using jwt

---
+++ middleware/is-auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  console.log(authHeader);
+  if (!authHeader) {
+    const error = new Error("Not authorized");
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.TOKEN);
+  }
+  catch (err) {
+    console.log(err);
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
+
