bbfd129e3f5ca5d2e688b0cfb0d2be6cafd5d28d
taras <tarasivashchuk@gmail.com>
2022-06-10 18:02:38 +0300

    added authorization to all feed routes

--- routes/feed.js
+++ routes/feed.js
@@ -4,9 +4,8 @@ const feedController = require("../controllers/feed");
 const { body } = require("express-validator");
 const isAuth = require("../middleware/is-auth");
 
-
 router.get("/posts", isAuth, feedController.getPosts);
-router.post("/post", [
+router.post("/post", isAuth, [
   body("title")
     .trim()
     .isLength({ min: 5 }),
@@ -15,8 +14,8 @@ router.post("/post", [
     .isLength({ min: 5 })
 ], feedController.createPost);
 
-router.get("/post/:postId", feedController.getPost);
-router.put("/post/:postId", [
+router.get("/post/:postId", isAuth, feedController.getPost);
+router.put("/post/:postId", isAuth, [
   body("title")
     .trim()
     .isLength({ min: 5 }),
@@ -24,6 +23,6 @@ router.put("/post/:postId", [
     .trim()
     .isLength({ min: 5 })
 ], feedController.updatePost);
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
